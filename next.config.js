edd5492270ff38df325274150bd47fc327056d1a
BITAN CHOWDHURY <121960216+Bitan1803@users.noreply.github.com>
2024-12-30 13:27:56 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com", // Replace with the domain of your image URL
+      },
+    ],
+  },
+};
